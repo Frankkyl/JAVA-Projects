3888eed9c5cb21fce34dfd887323f8ede820b375
kyfstll <liulin_sh@hotmail.com>
2017-05-05 18:38:25 -0700

    Create Member.java

---
+++ TankGame/MyTankGame3/Member.java
@@ -0,0 +1,90 @@
+package com.tankgame.TankGame3;
+
+/**
+ * Created by kysft on 5/5/17.
+ */
+
+public class Member {
+}
+class Tank3{
+    int x=0;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    int y=0;
+    public Tank3(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public int getDirect() {
+        return direct;
+    }
+
+    public void setDirect(int direct) {
+        this.direct = direct;
+    }
+
+    //up0,down1,left2,right3
+    int direct=0;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    int speed=10;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    int type=1;
+
+}
+
+class Enemy3 extends Tank3 {
+    public Enemy3(int x,int y){
+        super(x,y);
+    }
+}
+class Hero3 extends Tank3 {
+    public Hero3(int x,int y) {
+        super(x,y);
+    }
+    public void moveUp(){
+        y-=speed;
+    }
+    public void moveRight(){
+        x+=speed;
+    }
+    public void moveDown(){
+        y+=speed;
+    }
+    public void moveLeft(){
+        x-=speed;
+    }
+}
+
+
